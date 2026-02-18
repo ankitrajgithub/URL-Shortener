cadecd4ac3fa28648cfa1ba1595e1c7c1507c7be
Ankit Raj <ankit188raj.com>
2026-02-18 21:23:21 +0530

    Files added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       const response=await axios.post(`${API_BASE_URL}/shorten`,{
         originalUrl:url
       });
-      const newShortUrl=response.data.shortUrl;
+      const newShortUrl=response.data.url;
       setShortUrl(newShortUrl);
       setCopied(false);
       const qr=await QRCodeGenerator.toDataURL(newShortUrl);
@@ -39,27 +39,29 @@ function App() {
 
   return (
     <>
-      <div className='min-h-screen flex flex-col items-center justify-center p-6 gap-6'>
+      <div className='flex flex-col items-center justify-center p-6 gap-6'>
         <h1 className='text-4xl font-bold mb-4 text-center'>URL SHORTENER</h1>
         <div className="flex flex-col gap-3 w-full max-w-3xl">
-          <input type="text" className="input input-success w-full" placeholder="Enter long URL" value={url} onChange={(e)=>{setUrl(e.target.value)}}/>
+          <input type="text" className="input input-success w-full" placeholder="Enter long URL" value={url} onChange={(e)=>{
+            setUrl(e.target.value);
+          }}/>
           <button onClick={handleShorten} className='btn btn-primary w-full sm:auto'>Shorten</button>
         </div>
-      </div>
-      {shortUrl && (
-        <div className='flex flex-col items-center max-w-3xl w-full'>
-          <p className='font-medium mb-2'>Your Short Link:</p>
-          <a href={shortUrl} className='link link-primary break-all'>{shortUrl}</a>
-          <button className={`btn mt-2 w-full ${copied? "btn-success":"btn-secondary"}`} onClick={handleCopy}>{copied ? "Copied!" : "Copy"}</button>
-          <div className='bg-white p-4 rounded-lg shadow mt-6'>
-            <p className='mb-2 text-center font-semibold text-gray-800'>Scan QR code:</p>
-            <QRCode Value={shortUrl} size={180}/>
+        {shortUrl && (
+          <div className='flex flex-col items-center max-w-3xl w-full'>
+            <p className='font-medium mb-2 text-center'>Your Short Link:</p>
+            <a href={shortUrl} className='link link-primary break-all'>{shortUrl}</a>
+            <button className={`btn mt-2 w-full ${copied? "btn-success":"btn-secondary"}`} onClick={handleCopy}>{copied ? "Copied!" : "Copy"}</button>
+            <div className='bg-white p-4 rounded-lg shadow mt-6'>
+              <p className='mb-2 text-center font-semibold text-gray-800'>Scan QR code:</p>
+              <QRCode value={shortUrl} size={180}/>
+            </div>
+            {qrImage && (
+              <a className='btn btn-accent mt-3 w-full w-full text-center' href={qrImage} download="qr-code.png">Download QR Code</a>
+            )}
           </div>
-          {qrImage && (
-            <a className='btn btn-accent mt-3 w-full' href={qrImage} download="qr-code.png">Download QR Code</a>
-          )}
+        )}
         </div>
-      )}
     </>
   )
 }
