092f6154f16261d2a7f0dd0c8af5bcc7bcc98c93
Ankit Raj <ankit188raj.com>
2026-02-18 20:47:07 +0530

    Files added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,8 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import axios from "axios";
+const API_BASE_URL=import.meta.env.VITE_BACKEND_URL;
+import QRCode from 'react-qr-code';
+import QRCodeGenerator from 'qrcode';
 
 function App() {
   const [url,setUrl] = useState("");
@@ -8,13 +12,54 @@ function App() {
 
   const handleShorten=async ()=>{
     if(!url){
-      
+      return;
     }
+    try{
+      const response=await axios.post(`${API_BASE_URL}/shorten`,{
+        originalUrl:url
+      });
+      const newShortUrl=response.data.shortUrl;
+      setShortUrl(newShortUrl);
+      setCopied(false);
+      const qr=await QRCodeGenerator.toDataURL(newShortUrl);
+      setQrImage(qr);
+    }catch(e){
+      console.log(e);
+      alert("Something went wrong");
+    }
+  }
+
+  const handleCopy=()=>{
+    navigator.clipboard.writeText(shortUrl);
+    setCopied(true);
+    setTimeout(()=>{
+      setCopied(false)
+    },20000)
   }
 
   return (
     <>
-      <p>Hello</p>
+      <div className='min-h-screen flex flex-col items-center justify-center p-6 gap-6'>
+        <h1 className='text-4xl font-bold mb-4 text-center'>URL SHORTENER</h1>
+        <div className="flex flex-col gap-3 w-full max-w-3xl">
+          <input type="text" className="input input-success w-full" placeholder="Enter long URL" value={url} onChange={(e)=>{setUrl(e.target.value)}}/>
+          <button onClick={handleShorten} className='btn btn-primary w-full sm:auto'>Shorten</button>
+        </div>
+      </div>
+      {shortUrl && (
+        <div className='flex flex-col items-center max-w-3xl w-full'>
+          <p className='font-medium mb-2'>Your Short Link:</p>
+          <a href={shortUrl} className='link link-primary break-all'>{shortUrl}</a>
+          <button className={`btn mt-2 w-full ${copied? "btn-success":"btn-secondary"}`} onClick={handleCopy}>{copied ? "Copied!" : "Copy"}</button>
+          <div className='bg-white p-4 rounded-lg shadow mt-6'>
+            <p className='mb-2 text-center font-semibold text-gray-800'>Scan QR code:</p>
+            <QRCode Value={shortUrl} size={180}/>
+          </div>
+          {qrImage && (
+            <a className='btn btn-accent mt-3 w-full' href={qrImage} download="qr-code.png">Download QR Code</a>
+          )}
+        </div>
+      )}
     </>
   )
 }
